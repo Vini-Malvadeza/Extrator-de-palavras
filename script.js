baead99e7437b9a0b7f4d41180d5536a65d74cc8
Vini-Malvadeza <vinicius.pinheiro18@escola.pr.gov.br>
2025-08-24 23:04:25 -0300

    f

--- script.js
+++ script.js
@@ -12,13 +12,26 @@ campoResultado.textContent = palavrasChave.join(", ");
 
 function processaTexto(texto){
     let palavras = texto.split(/\P{L}+/u);
+    const frequencias = contaFrequencias(palavras);
+    let ordenadas = Object.Keys(frequencias).sort(ordenaPalavra);
+
+    function ordenaPalavra(p1, p2){
+        return frequencias[p2] - frequencias[p1];
+    }
+
+    console.log(ordenadas);
+    return ordenadas.slice(0,10);
+}
+
+
+function contaFrequencias(palavras){
     let frequencias = {};
     for (let i of palavras){
     frequencias [i] = 0;
     for (let j of palavras){
         if (i == j)
     }  frequencias [i]++;
-        console.log(frequencias);
+       return frequencias;
     }
-    return palavras;
+    
 }
